12ba3b505dd06280376a5a5b045b646dca0e8623
mjr2595 <michaeljohnraymond@gmail.com>
2024-04-15 23:36:34 -0500

    Update sumZero removed uneeded condition check

--- CS-Coding-Interview-Bootcamp/sumZero.js
+++ CS-Coding-Interview-Bootcamp/sumZero.js
@@ -2,7 +2,7 @@ function sumZero(arr) {
   let left = 0;
   let right = arr.length - 1;
 
-  while (left !== right && left < right) {
+  while (left < right) {
     let sum = arr[left] + arr[right];
     if (sum === 0) {
       return true;
